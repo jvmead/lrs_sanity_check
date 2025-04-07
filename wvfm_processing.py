99667ea5634b4dd7e051577b8ff959ea506084ba
James Mead <jvmead@login20.chn.perlmutter.nersc.gov>
2025-04-07 06:22:05 -0700

    honestly I dont remember but I need to merge

--- wvfm_processing.py
+++ wvfm_processing.py
@@ -267,7 +267,7 @@ def get_truth(filename, file_idx, in_tpc=False, n_photons_threshold=7500):
 # interaction finder function
 def interaction_finder(wvfm, noise,
                        n_noise_factor = 5.0,
-                       n_bins_rolled = 10,
+                       n_bins_rolled = 2,
                        n_sqrt_rt_factor = 5.0,
                        pe_weight = 1.0):
 
